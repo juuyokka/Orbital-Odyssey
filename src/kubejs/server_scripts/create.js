42bcd492c3a3d40364119f5f6ab93d7ec0e938fe
lactose <lactose@allthingslinux.org>
2025-11-24 11:55:47 -0800

    refactor(kubejs): refactor recipe declarations

--- src/kubejs/server_scripts/create.js
+++ src/kubejs/server_scripts/create.js
@@ -1,199 +1,252 @@
-// const of heat levels
-const HeatRequirement = {
+const klass = global.klass;
+
+if (!Array.prototype.flatMap) {
+  Array.prototype.flatMap = function (callbackFn, thisArg) {
+    return this.reduce(function (acc, val) {
+      const mappedVal = callbackFn.call(thisArg, val);
+      return acc.concat(mappedVal);
+    }, []);
+  };
+}
+
+const CreateIngredient = klass({
+  initialize: function (args) {
+    this.args = args;
+  },
+  serialize: function () {
+    throw new Error("Ingredient must implement a serialize function");
+  },
+});
+
+const CreateOutput = klass({
+  initialize: function (args) {
+    this.args = args;
+  },
+  serialize: function () {
+    let res = {};
+    if (this.args.chance != null) res.chance = this.args.chance;
+    return res;
+  },
+});
+
+const CreateItemStackIngredient = CreateIngredient.extend()
+  .statics({ id: "item" })
+  .methods({
+    serialize: function () {
+      let res = { item: this.args.item };
+      if (this.args.count != null) res.count = this.args.count;
+      return res;
+    },
+  });
+
+const CreateItemTagIngredient = CreateIngredient.extend()
+  .statics({ id: "item_tag" })
+  .methods({
+    serialize: function () {
+      let res = { tag: this.args.tag };
+      if (this.args.count != null) res.count = this.args.count;
+      return res;
+    },
+  });
+
+const CreateFluidStackIngredient = CreateIngredient.extend()
+  .statics({ id: "fluid" })
+  .methods({
+    serialize: function () {
+      return {
+        fluid: this.args.fluid,
+        type: "fluid_stack",
+        amount: this.args.amount,
+      };
+    },
+  });
+
+const CreateFluidTagIngredient = CreateIngredient.extend()
+  .statics({ id: "fluid_tag" })
+  .methods({
+    serialize: function () {
+      return {
+        fluid_tag: this.args.fluid_tag,
+        type: "fluid_tag",
+        amount: this.args.amount,
+      };
+    },
+  });
+
+const CreateItemStackOutput = CreateOutput.extend()
+  .statics({ id: "item" })
+  .methods({
+    serialize: function () {
+      let res = { id: this.args.item };
+      if (this.args.count != null) res.count = this.args.count;
+      return Object.assign(res, this.supr());
+    },
+  });
+
+const CreateFluidStackOutput = CreateOutput.extend()
+  .statics({ id: "fluid" })
+  .methods({
+    serialize: function () {
+      let res = { id: this.args.fluid, amount: this.args.amount };
+      return Object.assign(res, this.supr(this));
+    },
+  });
+
+const CreateIngredientMap = new Map(
+  [
+    CreateItemStackIngredient,
+    CreateItemTagIngredient,
+    CreateFluidStackIngredient,
+    CreateFluidTagIngredient,
+  ].map((i) => [i.id, i]),
+);
+
+const CreateOutputMap = new Map(
+  [CreateItemStackOutput, CreateFluidStackOutput].map((i) => [i.id, i]),
+);
+
+const CreateIngredientOutputMap = new Map(
+  [
+    [CreateItemStackIngredient, CreateItemStackOutput],
+    [CreateFluidStackIngredient, CreateFluidStackOutput],
+  ].flatMap(([ingredient, output]) => [
+    [ingredient, output],
+    [output, ingredient],
+  ]),
+);
+
+function makeCreateIngredient(args) {
+  const types = {
+    item: CreateIngredientMap.get("item"),
+    tag: CreateIngredientMap.get("item_tag"),
+    fluid: CreateIngredientMap.get("fluid"),
+    fluid_tag: CreateIngredientMap.get("fluid_tag"),
+  };
+
+  const key = Object.keys(types).find((k) => k in args);
+
+  return types[key];
+}
+
+function makeCreateOutput(args) {
+  const types = {
+    item: CreateOutputMap.get("item"),
+    fluid: CreateOutputMap.get("fluid"),
+  };
+
+  const key = Object.keys(types).find((k) => k in args);
+
+  return types[key];
+}
+
+function mergeWithArrays(target, source) {
+  return Object.entries(source).reduce((acc, [key, value]) => {
+    let res = {};
+    res[key] = Array.isArray(value) && Array.isArray(acc[key]) ? acc[key].concat(value) : value;
+    return Object.assign(acc, res);
+  }, target);
+}
+
+const RecipeType = klass({
+  initialize: function (id, recipeArguments) {
+    this.id = id;
+    this.recipeArguments = new Map(recipeArguments.map((a) => [a.id, a]));
+  },
+  build: function (args) {
+    const serializedArguments = Object.entries(args).map(([argKey, argValue]) => {
+      const ArgClass = this.recipeArguments.get(argKey);
+      let fallback = {};
+      fallback[argKey] = argValue;
+      return ArgClass == null ? fallback : new ArgClass(argValue).serialize();
+    });
+
+    return serializedArguments.reduce(mergeWithArrays, { type: this.id });
+  },
+});
+
+const RecipeArgument = klass({
+  initialize: function (args) {
+    this.data = args;
+  },
+  serialize: function () {
+    const ctor = this.constructor;
+    let res = {};
+    res[ctor.id] = this.data;
+
+    return res;
+  },
+});
+
+const CreateIngredientArgument = RecipeArgument.extend()
+  .statics({ id: "ingredients" })
+  .methods({
+    serialize: function () {
+      const chanceOutputs = this.data.flatMap((i) => {
+        if (i.return_chance == null || i.return_chance === 0) return [];
+        const createIngredient = makeCreateIngredient(i);
+        const createOutput = CreateIngredientOutputMap.get(createIngredient);
+
+        return createOutput != null
+          ? new createOutput(Object.assign(i, { chance: i.return_chance })).serialize()
+          : [];
+      });
+
+      let res = {
+        ingredients: this.data.map((i) => {
+          const createIngredient = makeCreateIngredient(i);
+          return new createIngredient(i).serialize();
+        }),
+      };
+
+      if (chanceOutputs.length > 0) res.results = chanceOutputs;
+
+      return res;
+    },
+  });
+
+const CreateOutputArgument = RecipeArgument.extend()
+  .statics({ id: "results" })
+  .methods({
+    serialize: function () {
+      return {
+        results: this.data.map((i) => {
+          const createOutput = makeCreateOutput(i);
+          return new createOutput(i).serialize();
+        }),
+      };
+    },
+  });
+
+// average online discourse
+const CreateHeatedArgument = RecipeArgument.extend().statics({
+  id: "heat_requirement",
   none: "none",
   heated: "heated",
   superheated: "superheated",
-};
+});
 
-// const of recipes
-const recipes = {
-  compacting: [
-    {
-      name: "steel",
-      heatRequirement: HeatRequirement.heated,
-      ingredients: [
-        { type: "item", id: "minecraft:coal" },
-        { type: "item", id: "minecraft:iron_ingot" },
-      ],
-      results: [{ type: "item", id: "stellaris:steel_ingot" }],
-    },
-  ],
-  mixing: [
-    {
-      name: "oil",
-      heatRequirement: HeatRequirement.none,
-      ingredients: [
-        { type: "fluid", id: "createdieselgenerators:crude_oil", amount: 250 },
-      ],
-      results: [{ type: "fluid", id: "stellaris:oil", amount: 250 }],
-    },
-    {
-      name: "insulating resin",
-      heatRequirement: HeatRequirement.none,
-      ingredients: [
-        { type: "fluid", id: "minecraft:water", amount: 500 },
-        { type: "item", id: "minecraft:bone_meal" },
-        { type: "item", id: "minecraft:glowstone_dust" },
-        { type: "item", id: "minecraft:slime_ball" },
-        { type: "tag", id: "c:silicon" },
-      ],
-      results: [{ type: "item", id: "appflux:insulating_resin" }],
-    },
-  ],
-};
+const CreateProcessingTimeArgument = RecipeArgument.extend().statics({
+  id: "processing_time"
+});
 
-// mixing function, takes in attributes from a recipe
-function mixing(event, name, heat, ingredients, results) {
-  let ing = []; // lists of json objects
-  let res = []; // to pass to event.custom
+const CreateMixingRecipe = new RecipeType("create:mixing", [
+  CreateHeatedArgument,
+  CreateIngredientArgument,
+  CreateOutputArgument,
+]);
 
-  for (let ingredient of ingredients) {
-    // check if type is undefined
-    if (ingredient.type == undefined) {
-      console.error(`Error parsing ingredients of ${name}: type is undefined!`);
-    } else if (ingredient.type == "") {
-      console.error(`Error parsing ingredients of ${name}: type is blank!`);
-    }
+const CreateCompactingRecipe = new RecipeType("create:compacting", [
+  CreateHeatedArgument,
+  CreateIngredientArgument,
+  CreateOutputArgument,
+]);
 
-    // check if id is undefined
-    if (ingredient.id == undefined) {
-      console.error(`Error parsing ingredients of ${name}: id is undefined!`);
-    } else if (ingredient.id == "") {
-      console.error(`Error parsing ingredients of ${name}: id is blank!`);
-    }
-
-    // parse ingredient and push json objects
-    let type = ingredient.type;
-    if (type == "item") {
-      ing.push({ item: ingredient.id });
-    } else if (type == "fluid") {
-      ing.push({
-        type: "fluid_stack",
-        amount: ingredient.amount,
-        fluid: ingredient.id,
-      });
-    } else if (type == "tag") {
-      ing.push({ tag: ingredient.id });
-    }
-  }
-
-  for (let result of results) {
-    // check if type is undefined
-    if (result.type == undefined) {
-      console.warn(
-        `Warning parsing results of ${name}: type is undefined! It is recommended to define a type for readabillity.`,
-      );
-    } else if (result.type == "") {
-      console.warn(
-        `Warning parsing results of ${name}: type is blank! It is recommended to define a type for readabillity.`,
-      );
-    }
-
-    // check if id is undefined
-    if (result.id == undefined) {
-      console.error(`Error parsing results of ${name}: id is undefined!`);
-    } else if (result.id == "") {
-      console.error(`Error parsing results of ${name}: id is blank!`);
-    }
-
-    // parse result and push json objects
-    if (result.amount != undefined) {
-      res.push({ amount: result.amount, id: result.id });
-    } else {
-      res.push({ id: result.id });
-    }
-  }
-
-  // create recipe from parsed and re-compiled objects
-  event.custom({
-    type: "create:mixing",
-    heat_requirement: heat,
-    ingredients: ing,
-    results: res,
-  });
-}
-
-// compacting function, takes in attributes from a recipie
-function compacting(event, name, heat, ingredients, results) {
-  let ing = []; // lists of json objects
-  let res = []; // to pass to event.custom
-
-  for (let ingredient of ingredients) {
-    // check if type is undefined
-    if (ingredient.type == undefined) {
-      console.error(`Error parsing ingredients of ${name}: type is undefined!`);
-    } else if (ingredient.type == "") {
-      console.error(`Error parsing ingredients of ${name}: type is blank!`);
-    }
-
-    // check if id is undefined
-    if (ingredient.id == undefined) {
-      console.error(`Error parsing ingredients of ${name}: id is undefined!`);
-    } else if (ingredient.id == "") {
-      console.error(`Error parsing ingredients of ${name}: id is blank!`);
-    }
-
-    // parse ingredient and push json objects
-    let type = ingredient.type;
-    if (type == "item") {
-      ing.push({ item: ingredient.id });
-    } else if (type == "fluid") {
-      ing.push({
-        type: "fluid_stack",
-        amount: ingredient.amount,
-        fluid: ingredient.id,
-      });
-    } else if (type == "tag") {
-      ing.push({ tag: ingredient.id });
-    }
-  }
-
-  for (let result of results) {
-    // check if type is undefined
-    if (result.type == undefined) {
-      console.warn(
-        `Warning parsing results of ${name}: type is undefined! It is recommended to define a type for readabillity.`,
-      );
-    } else if (result.type == "") {
-      console.warn(
-        `Warning parsing results of ${name}: type is blank! It is recommended to define a type for readabillity.`,
-      );
-    }
-
-    // check if id is undefined
-    if (result.id == undefined) {
-      console.error(`Error parsing results of ${name}: id is undefined!`);
-    } else if (result.id == "") {
-      console.error(`Error parsing results of ${name}: id is blank!`);
-    }
-
-    // parse result and push json objects
-    if (result.amount != undefined) {
-      res.push({ amount: result.amount, id: result.id });
-    } else {
-      res.push({ id: result.id });
-    }
-  }
-
-  // create recipe from parsed and re-compiled objects
-  // "what the hell dude, we could have just used datapacks!"
-  // - Xyno
-  event.custom({
-    type: "create:compacting",
-    heat_requirement: heat,
-    ingredients: ing,
-    results: res,
-  });
-}
-
-// const of recipe categories
-const categories = {
-  compacting: compacting,
-  mixing: mixing,
-};
+const CreateRecipeTypeMap = new Map(
+  [CreateMixingRecipe, CreateCompactingRecipe].map((i) => [i.id, i]),
+);
 
 ServerEvents.recipes((event) => {
-  // remove original mixing recipe for insulating resin by renaming the original shapeless recipe
+  // Remove Applied Flux insulating resin recipe and rename it so Create doesn't generate the mixing recipe
   event.remove({ id: "appflux:insulating_resin" });
   event
     .shapeless("appflux:insulating_resin", [
@@ -206,23 +259,35 @@ ServerEvents.recipes((event) => {
       "#c:dusts/glowstone",
     ])
     .id("appflux:insulating_resin_manual_only");
-  // loop through recipes and load them
-  for (let recipeCategory in recipes) {
-    for (let recipe of recipes[recipeCategory]) {
-      console.log(`Loading ${recipeCategory} recipe: ${recipe.name}`);
-      if (recipeCategory in categories) {
-        categories[recipeCategory](
-          event,
-          recipe.name,
-          recipe.heatRequirement,
-          recipe.ingredients,
-          recipe.results,
-        );
-      } else {
-        console.error(
-          `Error while loading recipe categories: ${recipeCategory} is not a valid recipe!`,
-        );
-      }
-    }
-  }
+
+  const recipes = {
+    "create:mixing": {
+      insulating_resin: {
+        ingredients: [
+          { fluid: "minecraft:water", amount: 500 },
+          { item: "minecraft:bone_meal" },
+          { item: "minecraft:glowstone_dust" },
+          { item: "minecraft:slime_ball" },
+          { tag: "c:silicon" },
+        ],
+        results: [{ item: "appflux:insulating_resin" }],
+      },
+      oil: {
+        ingredients: [{ fluid: "createdieselgenerators:crude_oil", amount: 250 }],
+        results: [{ fluid: "stellaris:oil", amount: 250 }],
+      },
+    },
+    "create:compacting": {
+      steel: {
+        heat_requirement: CreateHeatedArgument.heated,
+        ingredients: [{ item: "minecraft:coal" }, { item: "minecraft:iron_ingot" }],
+        results: [{ item: "stellaris:steel_ingot" }],
+      },
+    },
+  };
+
+  Object.entries(recipes).forEach(([recipeTypeId, recipes]) => {
+    const recipeType = CreateRecipeTypeMap.get(recipeTypeId);
+    Object.values(recipes).forEach((recipe) => event.custom(recipeType.build(recipe)));
+  });
 });
